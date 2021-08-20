4ed0262a87361b184e508f14af0e437333236d0c
David Balatero <dbalatero@gmail.com>
2021-08-20 15:45:25 -0400

    fix pairing mode big sur

--- hammerspoon/pairing-mode.lua
+++ hammerspoon/pairing-mode.lua
@@ -1,7 +1,30 @@
+-- local function enableDoNotDisturb()
+--   hs.execute("defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true")
+--   hs.execute("defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date \"`date -u +\"%Y-%m-%d %H:%M:%S +000\"`\"")
+--   hs.execute("killall NotificationCenter")
+-- end
+
 local function enableDoNotDisturb()
-  hs.execute("defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true")
-  hs.execute("defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date \"`date -u +\"%Y-%m-%d %H:%M:%S +000\"`\"")
-  hs.execute("killall NotificationCenter")
+  hs.osascript.applescript(
+    [[
+set checkboxName to "Do Not
+Disturb"
+
+tell application "System Events"
+  click menu bar item "Control Center" of menu bar 1 of application process "ControlCenter"
+  delay 0.1
+
+  set isChecked to (value of checkbox checkboxName of group 1 of group 1 of window "Control Center" of application process "ControlCenter")
+
+  if not (isChecked as boolean) then
+    click checkbox checkboxName of group 1 of group 1 of window "Control Center" of application process "ControlCenter"
+    delay 0.1
+  end if
+
+  click menu bar item "Control Center" of menu bar 1 of application process "ControlCenter"
+end tell
+    ]]
+  )
 end
 
 local function enablePairingMode()
