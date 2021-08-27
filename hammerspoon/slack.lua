93ba4cbdd15287376986ab81162706db3cb4b3c9
David Balatero <dbalatero@gmail.com>
2021-08-27 12:32:36 -0400

    Focus window

--- hammerspoon/slack.lua
+++ hammerspoon/slack.lua
@@ -1,5 +1,26 @@
+local function clearCursorFromMessageBox()
+  local app = hs.application.find("Slack")
+  if not app then return end
+
+  local window = app:mainWindow()
+  window:focus()
+
+  local frame = window:frame()
+  local click = {
+    x = frame.x + frame.w - 100,
+    y = frame.y + 20,
+  }
+
+  local previousMousePosition = hs.mouse.absolutePosition()
+
+  hs.eventtap.leftClick(click, 0)
+  hs.mouse.absolutePosition(previousMousePosition) -- restore it
+end
+
 local function focusSlackMessageBox()
-  hs.eventtap.keyStroke({'shift'}, 'F6', 500)
+  clearCursorFromMessageBox()
+
+  -- hs.eventtap.keyStroke({'shift'}, 'F6', 0)
 end
 
 local function openSlackReminder()
@@ -15,3 +36,47 @@ local function openSlackReminder()
 end
 
 hyperKey:bind('r'):toFunction("Slack /remind", openSlackReminder)
+
+slackModal = hs.hotkey.modal.new()
+
+local function slackUp()
+  hs.eventtap.keyStroke({}, 'up', 0)
+end
+
+local function slackDown()
+  hs.eventtap.keyStroke({}, 'down', 0)
+end
+
+local function slackThread()
+  focusSlackMessageBox()
+  slackUp()
+
+  hs.eventtap.keyStroke({}, 't', 0)
+
+  hs.timer.doAfter(0.3, function()
+    hs.eventtap.keyStroke({}, 'tab', 0)
+    hs.eventtap.keyStroke({}, 'tab', 0)
+    hs.eventtap.keyStroke({}, 'tab', 0)
+    hs.eventtap.keyStroke({}, 'tab', 0)
+    hs.eventtap.keyStroke({}, 'tab', 0)
+    hs.eventtap.keyStroke({}, 'tab', 0)
+    hs.eventtap.keyStroke({}, 'tab', 0)
+  end)
+end
+
+-- slackModal:bind({'ctrl'}, 'h', nil, focusSlackMessageBox, nil, focusSlackMessageBox)
+slackModal:bind({'ctrl'}, 'j', nil, slackDown, nil, slackDown)
+slackModal:bind({'ctrl'}, 'k', nil, slackUp, nil, slackUp)
+slackModal:bind({'ctrl'}, 't', nil, slackThread, nil, slackThread)
+
+slackWatcher = hs.application.watcher.new(function(applicationName, eventType)
+  if applicationName ~= "Slack" then return end
+
+  if eventType == hs.application.watcher.activated then
+    slackModal:enter()
+  elseif eventType == hs.application.watcher.deactivated then
+    slackModal:exit()
+  end
+end)
+
+slackWatcher:start()
