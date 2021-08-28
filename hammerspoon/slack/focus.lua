d56d95e6695c885bf78e05425b13eaadbf0256e9
David Balatero <dbalatero@gmail.com>
2021-08-28 14:40:41 -0400

    Full vim nav keys

---
+++ hammerspoon/slack/focus.lua
@@ -0,0 +1,85 @@
+local find = require('slack.find')
+
+----------
+
+local function getAxSlackWindow()
+  local app = hs.application.find("Slack")
+  if not app then return end
+
+  local window = app:mainWindow()
+  window:focus()
+
+  return hs.axuielement.windowElement(window)
+end
+
+local function hasClass(element, class)
+  local classList = element:attributeValue('AXDOMClassList')
+  if not classList then return false end
+
+  return hs.fnutils.contains(classList, class)
+end
+
+-----------
+
+local module = {}
+
+module.mainMessageBox = function()
+  local window = getAxSlackWindow()
+  if not window then return end
+
+  local textarea = find.searchByChain(window, {
+    function(elem) return hasClass(elem, 'p-workspace-layout') end,
+    function(elem) return elem:attributeValue('AXSubrole') == 'AXLandmarkMain' end,
+    function(elem) return hasClass(elem, 'p-workspace__primary_view_contents') end,
+    function(elem) return hasClass(elem, 'c-wysiwyg_container') end,
+    function(elem) return elem:attributeValue('AXRole') == 'AXTextArea' end,
+  })
+
+  if textarea then
+    textarea:setAttributeValue('AXFocused', true)
+  end
+end
+
+module.threadMessageBox = function(withRetry)
+  withRetry = withRetry or false
+
+  local window = getAxSlackWindow()
+  if not window then return end
+
+  local findTextarea = function()
+    return find.searchByChain(window, {
+      function(elem) return hasClass(elem, 'p-workspace-layout') end,
+      function(elem) return hasClass(elem, 'p-flexpane') end,
+      function(elem) return hasClass(elem, 'p-threads_flexpane') end,
+      function(elem) return hasClass(elem, 'c-wysiwyg_container') end,
+      function(elem) return elem:attributeValue('AXRole') == 'AXTextArea' end,
+    })
+  end
+
+  local textarea = nil
+
+  local textareaVisible = function()
+    textarea = findTextarea()
+    return not not textarea
+  end
+
+  local focusTextarea = function()
+    textarea:setAttributeValue('AXFocused', true)
+  end
+
+  if withRetry then
+    -- Do it in a retry loop
+    local loopTimer = hs.timer.waitUntil(textareaVisible, focusTextarea)
+
+    -- Give up after 2 seconds
+    hs.timer.doAfter(2, function()
+      loopTimer:stop()
+    end)
+  elseif textareaVisible() then
+    -- fire it once
+    focusTextarea()
+  end
+end
+
+
+return module
---
+++ hammerspoon/slack/init.lua
@@ -0,0 +1,53 @@
+local focus = require('slack.focus')
+
+local function slackUp()
+  hs.eventtap.keyStroke({}, 'up', 0)
+end
+
+local function slackDown()
+  hs.eventtap.keyStroke({}, 'down', 0)
+end
+
+local function openSlackReminder()
+  hs.application.launchOrFocus("Slack")
+
+  hs.timer.doAfter(0.3, function()
+    focus.mainMessageBox()
+
+    hs.timer.doAfter(0.3, function()
+      hs.eventtap.keyStrokes("/remind me at ")
+    end)
+  end)
+end
+
+local function openSlackThread()
+  focus.mainMessageBox()
+
+  hs.timer.doAfter(0.1, function()
+    slackUp()
+    hs.eventtap.keyStroke({}, 't', 0)
+    focus.threadMessageBox(true)
+  end)
+end
+
+slackModal = hs.hotkey.modal.new()
+
+slackModal:bind({'ctrl'}, 'h', nil, focus.mainMessageBox, nil, focus.mainMessageBox)
+slackModal:bind({'ctrl'}, 'j', nil, slackDown, nil, slackDown)
+slackModal:bind({'ctrl'}, 'k', nil, slackUp, nil, slackUp)
+slackModal:bind({'ctrl'}, 'l', nil, focus.threadMessageBox, nil, focus.threadMessageBox)
+slackModal:bind({'ctrl'}, 't', nil, openSlackThread, nil, openSlackThread)
+
+slackWatcher = hs.application.watcher.new(function(applicationName, eventType)
+  if applicationName ~= "Slack" then return end
+
+  if eventType == hs.application.watcher.activated then
+    slackModal:enter()
+  elseif eventType == hs.application.watcher.deactivated then
+    slackModal:exit()
+  end
+end)
+
+slackWatcher:start()
+
+hyperKey:bind('r'):toFunction("Slack /remind", openSlackReminder)
