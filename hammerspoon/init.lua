247d4fb510e16e7e141f1081fbc11029adc0711c
David Balatero <dbalatero@gmail.com>
2021-09-16 12:32:59 -0400

    Disable autocomplete

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -20,7 +20,7 @@ require "ocr-paste"
 require "zoom"
 -- require "text-expander"
 require "course"
-require "autocomplete"
+-- require "autocomplete"
 require "jira-markdown"
 -- require "read-later"
 
